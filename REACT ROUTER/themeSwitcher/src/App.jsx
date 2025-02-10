f7106f2ce8d787b90fd60459b1d010eff7572081
Akshesh Sabhadiya <aksheshsabhadiya2021@gmail.com>
2025-02-10 11:17:27 +0530

    toggle-done

--- REACT ROUTER/themeSwitcher/src/App.jsx
+++ REACT ROUTER/themeSwitcher/src/App.jsx
@@ -25,14 +25,18 @@ function App() {
 
   return (
     <ThemeProvider value={{themeMode, darkTheme, lightTheme}}>
+      <div className={themeMode === 'dark' ? "bg-black flex flex-wrap min-h-screen items-center" : "bg-white flex flex-wrap min-h-screen items-center"}>
       <div className="flex flex-wrap min-h-screen items-center">
         <div className="w-full">
           <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
             <ThemeBtn />
           </div>
 
-          <div className="w-full max-w-sm mx-auto">
+          {/* <div className=> */}
+          {/* <div className={themeMode === 'light' ? "bg-black" : "bg-white"}> */}
+          <div className='w-full max-w-sm mx-auto'>
             <Card />
+          </div>
           </div>
         </div>
       </div>
--- REACT ROUTER/themeSwitcher/src/Components/Card.jsx
+++ REACT ROUTER/themeSwitcher/src/Components/Card.jsx
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 export default function Card() {
+
+    const ele = document.querySelector('html').classList
+    console.log("Element : ",ele.value);
+
     return (
-        <div className="w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        // <div className="w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <div className={ele.value === 'light' ? "w-full bg-white border text-black border-gray-200 rounded-lg shadow" : "bg-black text-white w-full border border-gray-200 rounded-lg shadow"}>
             <a href="/">
                 <img className="p-8 rounded-t-lg" src="https://cdn.pixabay.com/photo/2021/11/14/18/36/telework-6795505_1280.jpg" alt="product_image1" />
             </a>
             <div className="px-5 pb-5">
                 <a href="/">
-                    <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                    <h5 className="text-xl font-semibold tracking-tight">
                         Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
                     </h5>
                 </a>
@@ -63,7 +68,7 @@ export default function Card() {
                     </span>
                 </div>
                 <div className="flex items-center justify-between">
-                    <span className="text-3xl font-bold text-gray-900 dark:text-white">$599</span>
+                    <span className="text-3xl font-bold">$599</span>
                     <a
                         href="/"
                         className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
