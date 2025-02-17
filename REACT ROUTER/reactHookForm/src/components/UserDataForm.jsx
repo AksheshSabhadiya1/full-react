37499503c11371c6868c14cae75a346ab58445ec
Akshesh Sabhadiya <aksheshsabhadiya2021@gmail.com>
2025-02-17 12:18:21 +0530

    mode, validation

--- REACT ROUTER/reactHookForm/src/components/UserDataForm.jsx
+++ REACT ROUTER/reactHookForm/src/components/UserDataForm.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from "react";
-import {useForm, useFieldArray} from 'react-hook-form'
-import { DevTool} from '@hookform/devtools'
+import React, { useEffect } from "react";
+import { useForm, useFieldArray } from 'react-hook-form'
+import { DevTool } from '@hookform/devtools'
 
-const UserDataForm = () =>{
+const UserDataForm = () => {
 
     const form = useForm({
         // defaultValues: {
@@ -25,39 +25,46 @@ const UserDataForm = () =>{
                     instagram: "",
                     facebook: "",
                 },
-                mobile: ["",""],                //array
-                phNumbers: [{ number: ''}],
-                hobbies: [{ hobby: ''}],
+                mobile: ["", ""],                //array
+                phNumbers: [{ number: '' }],
+                hobbies: [{ hobby: '' }],
                 age: '',
                 DOB: new Date(),
             }
-        }
+        },
+
+        // mode: 'onBlur'               //when Focus on field, error msg show & when enter continue appear
+        mode: 'onTouched'               //when Focus on field, error msg show & when enter input data msg dissappear
+        // mode: 'onChange'             //when enter input data check validation for each input
+        // mode: 'all'                  //combination of onBlur & onChange mode
     })
 
     const onFormSubmit = (data) => {
-        console.log("Form Submitted",data);
+        alert("Form Sucessfully Submitted")
+        console.log("Form Submitted", data);
     }
 
 
 
     //~ Destructuring form
-        //type-1
-    const { register,                                           
-            control, 
-            handleSubmit, 
-            formState, 
-            watch, 
-            getValues, 
-            setValue,
-            reset,      } = form            
-        
-        //type-2
+    //type-1
+    const { register,
+        control,
+        handleSubmit,
+        formState,
+        watch,
+        getValues,
+        setValue,
+        reset,
+        trigger,     } = form
+
+    //type-2
     // const {name, ref, onChange, onBlur} = register("username")             
 
 
 
     //~ error handling
-    const {errors} = formState      
+    const { errors } = formState
 
 
 
@@ -67,7 +74,7 @@ const UserDataForm = () =>{
         control
     })
 
-    const { fields : data, append : appendData, remove : removeData } = useFieldArray({
+    const { fields: data, append: appendData, remove: removeData } = useFieldArray({
         name: 'hobbies',
         control
     })
@@ -93,19 +100,18 @@ const UserDataForm = () =>{
 
 
     //~ getValues   :- prevent against re-rendering of form
-    const handleGetValues = () =>{
-       console.log("Get Values: ",getValues())
-       console.log("Get Username: ",getValues("username"))
-       console.log("Get Username & email: ",getValues(["username",'email']))
-       console.log("Get Social: ",getValues('social'))
-       console.log("Get Social: ",getValues('social.instagram'))
+    const handleGetValues = () => {
+        console.log("Get Values: ", getValues())
+        console.log("Get Username: ", getValues("username"))
+        console.log("Get Username & email: ", getValues(["username", 'email']))
+        console.log("Get Social: ", getValues('social'))
     }
 
 
 
     //~ setValue    :- set value of field and also apply touched, dirty and validate
     const handleSetValue = () => {
-        setValue('username','',{
+        setValue('username', 'Akshesh123', {
             shouldValidate: true,
             shouldDirty: true,
             shouldTouch: true
@@ -115,33 +121,33 @@ const UserDataForm = () =>{
 
 
     //~ touchedFields, dirtyFields, isDirty, isvalid
-    const {touchedFields, dirtyFields, isDirty, isValid} = formState
+    const { touchedFields, dirtyFields, isDirty, isValid } = formState
 
-    console.log("touchedFields: ",touchedFields,"dirtyFields: ",dirtyFields,"isDirty: ",isDirty,"isValid: ", isValid);
-    
+    console.log("touchedFields: ", touchedFields, "dirtyFields: ", dirtyFields, "isDirty: ", isDirty, "isValid: ", isValid);
+
 
 
     //~ isSubmitting, isSubmitted, isSubmitSuccessful, submitCount
-    const {isSubmitting, isSubmitted, isSubmitSuccessful, submitCount} = formState
+    const { isSubmitting, isSubmitted, isSubmitSuccessful, submitCount } = formState
 
-    console.log("isSubmitting: ",isSubmitting,"isSubmitted: ",isSubmitted,"isSubmitSuccessful: ", isSubmitSuccessful, "submitCount: ", submitCount);
+    console.log("isSubmitting: ", isSubmitting, "isSubmitted: ", isSubmitted, "isSubmitSuccessful: ", isSubmitSuccessful, "submitCount: ", submitCount);
 
 
     //~ Handle FieldError when submiting form
     const onError = (error) => {
-        console.log("Form Submiting Error ",error);
+        console.log("Form Submiting Error ", error);
     }
 
 
 
     //~ Reset form data
-    useEffect(()=>{
+    useEffect(() => {
 
-        if(isSubmitSuccessful){
+        if (isSubmitSuccessful) {
             reset()
         }
 
-    },[isSubmitSuccessful,reset])
+    }, [isSubmitSuccessful, reset])
 
 
 
@@ -152,9 +158,11 @@ const UserDataForm = () =>{
             {/* <h3>Watch All Value: {JSON.stringify(watchdata)}
                <p>Watch Value: {watchusername}</p>
             </h3> */}
+            
             <form onSubmit={handleSubmit(onFormSubmit, onError)} noValidate >
+
                 <label htmlFor="username">Username</label>
-                <input type="text" id="username" {...register("username",{required : 'Username is required'})} />
+                <input type="text" id="username" {...register("username", { required: 'Username is required' })} />
                 <p className="error">{errors.username?.message}</p>
                 {/* <input type="text"
                        id="username"
@@ -165,50 +173,53 @@ const UserDataForm = () =>{
 
                 <label htmlFor="email">Email</label>
                 <input type="email" id="email" {...register("email",
-                                                           { required : 'Email is required', 
-                                                             pattern: {
-                                                                value : /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
-                                                                message: "Invalid Email Format"
-                                                            },
-                                                            emailAvailable: async (fieldvalue) => {
-                                                                const response = await fetch(`https://jsonplaceholder.typicode.com/users?email=${fieldvalue}`)
-                                                                const data = await response.json()
+                    {
+                        required: 'Email is required',
+                        pattern: {
+                            value: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                            message: "Invalid Email Format"
+                        },
+                        validate: {
+                            emailAvailable: async (fieldvalue) => {
+                                const response = await fetch(`https://jsonplaceholder.typicode.com/users?email=${fieldvalue}`)
+                                const data = await response.json()
 
-                                                                return data.length == 0 || "Email already exists"
-                                                            },  
-                                                                                                  
-                                                        })} 
-                                                             />
+                                return data.length == 0 || "Email already exists"
+                            },
+                        },
+                    }
+                )} />
                 <p className="error">{errors.email?.message}</p>
 
                 <label htmlFor="channel">Channel</label>
-                <input type="text" id="channel" {...register("channel",{required: 'Channel is required'})} />
+                <input type="text" id="channel" {...register("channel", { required: 'Channel is required' })} />
                 <p className="error">{errors.channel?.message}</p>
 
                 <label htmlFor="instagram">Instagram</label>
-                <input type="text" id="instagram" {...register("social.instagram",{required: 'Instagram Id is required'})} />
+                <input type="text" id="instagram" {...register("social.instagram", { required: 'Instagram Id is required' })} />
                 <p className="error">{errors.social?.instagram?.message}</p>
 
                 <label htmlFor="facebook">Facebook</label>
-                <input type="text" id="facebook" {...register("social.facebook",{required: 'Facebook Id is required' })} />
+                <input type="text" id="facebook" {...register("social.facebook", { required: 'Facebook Id is required' })} />
                 <p className="error">{errors.social?.facebook?.message}</p>
 
                 <label htmlFor="PrimaryNumber">Primary Number</label>
-                <input type="text" id="PrimaryNumber" maxLength={10} {...register("mobile.0",{required: 'Primary Number is required'})} />
+                <input type="text" id="PrimaryNumber" maxLength={10} {...register("mobile.0", { required: 'Primary Number is required' })} />
                 <p className="error">{errors.mobile?.[0]?.message}</p>
 
                 <label htmlFor="SecondaryNumber">Secondary Number</label>
-                <input type="text" id="SecondaryNumber" maxLength={10} {...register("mobile.1",{ required: 'Secondary Number is required',
-                                                                                                //  disabled: watch('mobile.0') === ''
+                <input type="text" id="SecondaryNumber" maxLength={10} {...register("mobile.1", {
+                    required: 'Secondary Number is required',
+                    //  disabled: watch('mobile.0') === ''
                 })} />
                 <p className="error">{errors.mobile?.[1]?.message}</p>
 
                 <label htmlFor="age">Age</label>
-                <input type="number" id="age" {...register("age",{required: 'age is required', valueAsNumber: true})} />
+                <input type="number" id="age" {...register("age", { required: 'age is required', valueAsNumber: true })} />
                 <p className="error">{errors.age?.message}</p>
 
                 <label htmlFor="DOB">Date OF Birth</label>
-                <input type="date" id="DOB" {...register("DOB",{required: 'Date Of Birth is required', valueAsDate: true})} />
+                <input type="date" id="DOB" {...register("DOB", { required: 'Date Of Birth is required', valueAsDate: true })} />
                 <p className="error">{errors.DOB?.message}</p>
 
 
@@ -217,14 +228,17 @@ const UserDataForm = () =>{
                     <div>
                         {
                             fields.map((field, index) => {
-                                return(
-                                <div className="form-control" key={field.id}>
-                                    <input type="text" {...register(`phNumbers.${index}.number`)} />
-                                    <button type="button" onClick={() => remove(index)}> Remove number</button>
-                                </div>)
+                                return (
+                                    <div className="form-control" key={field.id}>
+                                        <input type="text" {...register(`phNumbers.${index}.number`)} />
+                                        {
+                                            index > 0 && (<button type="button" onClick={() => remove(index)}> Remove</button>)
+                                        }
+
+                                    </div>)
                             })
                         }
-                        <button type="button" onClick={() => append({ number: ''})}>Add number</button>
+                        <button type="button" onClick={() => append({ number: '' })}>Add New number</button>
                     </div>
                 </div>
 
@@ -234,25 +248,29 @@ const UserDataForm = () =>{
                     <label>List your Hobbies</label>
                     <div>
                         {
-                            data.map((field2,index)=>{
-                                return(
+                            data.map((field2, index) => {
+                                return (
                                     <div className="form-control" key={field2.id}>
                                         <input type="text" {...register(`hobbies.${index}.hobby`)} />
-                                        <button type="button" onClick={()=>removeData(index)}>Remove Hobby</button>
+                                        {
+                                            index > 0 && (<button type="button" onClick={() => removeData(index)}>Remove</button>)
+                                        }
+
                                     </div>
                                 )
                             })
                         }
-                        <button type="button" onClick={()=>appendData({ hobby: ''})}>Add Hobby</button>
+                        <button type="button" onClick={() => appendData({ hobby: '' })}>Add New Hobby</button>
                     </div>
                 </div>
-                         
-                <button disabled={!isDirty || isSubmitting}>Submit</button>
+
+                <button disabled={!isDirty || isSubmitting || !isValid}>Submit</button>
                 <button type="button" onClick={handleGetValues}>Get Values</button>
                 <button type="button" onClick={handleSetValue}>Set Value</button>
                 <button type="button" onClick={() => reset()}>Reset</button>
+                <button type="button" onClick={() => trigger()}>validate</button>
             </form>
-            <DevTool control={control}/>
+            <DevTool control={control} />
         </div>
     )
 }
