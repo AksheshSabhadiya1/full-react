3dcc1f3608f2e048f954fd8224f4b5b33c78b7a6
Akshesh Sabhadiya <aksheshsabhadiya2021@gmail.com>
2025-02-18 22:36:16 +0530

    login, signup & authlayout

---
+++ REACT ROUTER/megaBlog/src/components/AuthLayout.jsx
@@ -0,0 +1,31 @@
+import React,{useState, useEffect} from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+function Protected({children, authentication=true}) {
+
+    const [loader, setLoder] = useState(true)
+    const authStatus = useSelector((state) => state.auth.status)
+    const navigate = useNavigate()
+
+    useEffect(()=>{
+
+        // if(authStatus === true){
+        //     navigate('/')
+        // }else if(authStatus === false){
+        //     navigate('/login')
+        // }
+
+        if(authentication && authentication !== authStatus){
+            navigate('/login')
+        } else if(!authentication && authStatus !== authentication){
+            navigate('/')
+        }
+        setLoder(false)
+
+    },[authStatus, authentication, navigate])
+
+    return loader ? <h1>Loading....</h1> : <>{children}</>
+}
+
+export default Protected
--- REACT ROUTER/megaBlog/src/components/Signup.jsx
+++ REACT ROUTER/megaBlog/src/components/Signup.jsx
@@ -1,26 +1,25 @@
 import React, { useState } from "react";
-import { Link, useNavigate } from 'react-router-dom'
-import { login as authLogin } from '../store/authSlice'
-import { Button, Input, Logo } from './index'
-import { useDispatch } from "react-redux";
 import authservice from "../appwrite/auth_service";
-import { useForm } from 'react-hook-form'
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { login } from "../store/authSlice";
+import { Input, Button, Logo } from './index'
+import { useForm } from "react-hook-form";
 
-function Login() {
+function Signup() {
 
+    const [error, setError] = useState('')
     const navigate = useNavigate()
     const dispatch = useDispatch()
-
     const { register, handleSubmit } = useForm()
-    const [error, setError] = useState('')
 
-    const login = async (data) => {
+    const signup = async (data) => {
         setError('')
         try {
-            const session = await authservice.login(data)
-            if (session) {
+            const sessiondata = await authservice.createAccount()
+            if (sessiondata) {
                 const userData = await authservice.getCurrentUser()
-                if (userData) dispatch(authLogin(userData))
+                if (userData) dispatch(login(userData))
                 navigate('/')
             }
 
@@ -30,28 +29,36 @@ function Login() {
     }
 
     return (
-        <div className="flex items-center justify-center w-full" >
-            <div className="mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10">
+        <div className="flex items-center justify-center">
+            <div className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}>
                 <div className="mb-2 flex justify-center">
                     <span className="inline-block w-full max-w-[100px]">
                         <Logo width="100%" />
                     </span>
                 </div>
-                <h2 className="text-center text-2xl font-bold leading-tight">Sign in your account</h2>
+                <h2 className="text-center text-2xl font-bold leading-tight">Sign up to create account</h2>
                 <p className="mt-2 text-center text-base text-black/60">
-                    Don&apos;t have any account?&nbsp;
+                    Already have an account?&nbsp;
                     <Link
-                        to="/signup"
+                        to="/login"
                         className="font-medium text-primary transition-all duration-200 hover:underline"
                     >
-                        Sign Up
+                        Sign In
                     </Link>
                 </p>
                 {
                     error && <p className="text-red-600 mt-8 text-center">{error}</p>
                 }
-                <form onSubmit={handleSubmit(login)} className="mt-8">
-                    <div className="space-y-5">
+
+                <form onSubmit={handleSubmit(signup)}>
+                    <div className='space-y-5'>
+                        <Input
+                            label='FullName: '
+                            placeholder='Enter Your FullName'
+                            {...register('FullName', {
+                                required: true
+                            })}
+                        />
                         <Input
                             label='Email: '
                             placeholder='Enter Your Email'
@@ -77,7 +84,7 @@ function Login() {
                         <Button
                             type="submit"
                             className="w-full"
-                        >Sign in</Button>
+                        >Create Account</Button>
                     </div>
                 </form>
             </div>
@@ -85,4 +92,4 @@ function Login() {
     )
 }
 
-export default Login
+export default Signup
